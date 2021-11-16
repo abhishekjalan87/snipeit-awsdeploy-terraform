8ecdaa6b2be8b553917167b34b5488f9cbf0c640
Abhishek Jalan <abhishek.jalan84@gmail.com>
2021-11-16 22:16:38 +0100

    updated installer.tf

--- SnipeIT_AWS_Terraform/installer.tf
+++ SnipeIT_AWS_Terraform/installer.tf
@@ -34,7 +34,12 @@ resource "aws_instance" "rts" {
       "echo ''",
       "echo ${self.private_ip}",
       "sudo hostnamectl set-hostname snipeit",
-
+      "sudo mkdir /root/docker-data/",
+      "sudo systemctl stop firewalld",
+      "sudo systemctl disable firewalld",
+      "sudo docker run --name snipe-mysql --restart=always -d -e MYSQL_ROOT_PASSWORD=test@123 -e MYSQL_DATABASE=snipe -e MYSQL_USER=snipe -e MYSQL_PASSWORD=test@123 -e TZ=Europe/Berlin -p 127.0.0.1:3306:3306 -v /root/docker-data/snipe-mysql:/var/lib/mysql mysql:5.7.30 --sql-mode="" ",
+      "sudo docker run -d --name=snipe-it --restart=always --link snipe-mysql:db -e MAIL_PORT_587_TCP_ADDR=smtp.gmail.com -e MAIL_PORT_587_TCP_PORT=465 -e MAIL_ENV_FROM_ADDR=test@test.de -e MAIL_ENV_FROM_NAME=IT-Admin -e MAIL_ENV_ENCRYPTION=ssl -e MAIL_ENV_USERNAME=test@test.de -e MAIL_ENV_PASSWORD=kuieddfggvjbm -e DB_CONNECTION=mysql -e DB_HOST=snipe-mysql -e DB_DATABASE=snipe -e DB_USERNAME=snipe -e DB_PASSWORD=test@123 -e APP_TIMEZONE=Europe/Berlin -e APP_KEY=base64:5U/KPKw1GN/Rz0fWYO/4FsSOqjmjvDAQzMCqwcAqstc= -v /root/docker-data/snipe-conf:/var/lib/snipeit -p 80:80 snipe/snipe-it:v4.9.2",
+      "sudo docker ps -a",
       "echo ''",
     ]
 
